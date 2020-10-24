dc752efe8f4932013aa60049f96700640b3247e2
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2020-10-24 14:37:53 +0200

    Prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/database');
+const path = require('path');
 
 const app = express();
 
@@ -21,6 +22,16 @@ app.use(cors());
 
 // Mount routers
 app.use('/api/recipes', recipes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
